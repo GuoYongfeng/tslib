ccb2881669c034ce85bae6e61779f7b133191142
guoyongfeng <guoyff@yonyou.com>
2020-08-28 10:36:00 +0800

    add type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 let a: string = "typescript library"
+let num: number = 123
+let bool: boolean = false;
+
+let numNew: number;
+numNew = 123;
+
+let str: string = "Lison";
+
+let strA: 'Lison'
+
+// console.log(strA)
+
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
+let list3: number|string[] 
 
 const el = document.createElement('div')
  el.innerText = a
